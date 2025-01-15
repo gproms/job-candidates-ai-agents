13e8cf726eed01397c8090c17e74dc0539eeb47a
Jorge Prombonas <jorge.prombonas@gmail.com>
2025-01-15 03:35:04 +0200

    reformat

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 # Absolute path to the project root directory
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 # Absolute path to the data directory
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
+DATA_DIR = os.path.join(ROOT_DIR, "data")
