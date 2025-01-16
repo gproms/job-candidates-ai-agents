11f743a75df9db88b6e69eb2d5bd8d440f908d69
Jorge Prombonas <jorge.prombonas@gmail.com>
2025-01-16 04:21:35 +0200

    try and except for file loader

--- src/utils/file_loader.py
+++ src/utils/file_loader.py
@@ -4,20 +4,13 @@ def load_json(filepath):
     """
     Load a JSON file and return its data.
     """
-    try:
-        with open(filepath, "r") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Error loading JSON file: {e}")
-        return {}
+    with open(filepath, "r") as file:
+        return json.load(file)
 
 def export_to_json(data, filename):
     """
     Export data to a JSON file.
     """
-    try:
-        with open(filename, "w") as json_file:
-            json.dump(data, json_file, indent=2)
-        print(f"Results saved to {filename}")
-    except IOError as e:
-        print(f"Error saving JSON file: {e}")
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=2)
+    print(f"Results saved to {filename}")
